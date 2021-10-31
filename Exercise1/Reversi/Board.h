f2d5f49d2f39e553e3e60aa6e6b12515f4fc50a9
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2021-10-31 17:40:39 +0200

    Include implementation methods

--- Exercise1/Reversi/Board.h
+++ Exercise1/Reversi/Board.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #include <array>
-#include<list>
-#include "Move.h"
+#include <list>
+#include <utility>
+#include "Position.h"
 
 using namespace std;
 
@@ -15,28 +16,53 @@ A class describing the game's board.
 class Board {
 
 public:
-	Board();
 	Board(const Board& oldBoard);
-	~Board();
 
 	/*
 	Returns all the valid moves in this board for the next player.
 	*/
-	list<Move> getValidMoves();
+	list<Position> get_valid_moves(PLAYER p);
 
 	/*
 	Returns whether or not a move would be valid.
 	*/
-	bool isValidMove(Move& move);
+	bool is_valid_move(PLAYER, Position);
 
 	/*
 	Marks a player's move on the board.
 	*/
-	void makeMove(PLAYER p, Move& move);
+	void make_move(PLAYER, Position) ;
+
+	string to_string();
 
 private:
 	const static int DIMENSION = 8; //cant make this non-static AND have an array with fixed size
 	array<array<PLAYER, DIMENSION>, DIMENSION> board;
 
+	bool range_is_valid(Position);
+
+	/*
+	Get a pair containing the furthest pair of player controlled squares in a line.
+	Returns an invalid position if a pair isn't found.
+	*/
+	pair<Position, Position> limits_in_x(PLAYER p, int line);
+	
+	/*
+	Get a pair containing the furthest pair of player controlled squares in a row.
+	Returns an invalid position if a pair isn't found.
+	*/
+	pair<Position, Position> limits_in_y(PLAYER p, int row);
+
+	/*
+	Get a pair containing the furthest pair of player controlled squares in the move's square's
+	main diagonal. Returns an invalid position if a pair isn't found.
+	*/
+	pair<Position, Position> limits_in_main_diag(PLAYER, Position);
+
+	/*
+	Get a pair containing the furthest pair of player controlled squares in the move's square's
+	secondary diagonal. Returns an invalid position if a pair isn't found.
+	*/
+	pair<Position, Position> limits_in_sec_diag(PLAYER, Position);
 };
 
