37635efcda7a4b481ce2ce81cf8c8e943235e4b8
AAAlex-123 <alexmandelias@gmail.com>
2021-11-08 16:28:12 +0200

    implement original HC

---
+++ Exercise1/N_Queen_Problem/StateSearcher.cpp
@@ -0,0 +1,28 @@
+#include "StateSearcher.h"
+
+State *StateSearcher::HillClimbing(State *initial_state)
+{
+	State *current = initial_state;
+
+	bool exists_larger = true;
+	while (exists_larger) {
+
+		exists_larger = false;
+		State *largest = current;
+
+		for (auto iter = current->begin(), end = current->end(); iter != end; iter++) {
+			if ((*iter)->getScore() > largest->getScore()) {
+				delete largest;
+				largest = *iter;
+				exists_larger = true;
+			}
+			else {
+				delete *iter;
+			}
+		}
+		current = largest;
+	}
+
+	return current;
+}
+
