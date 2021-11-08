120a78dff76ee1942591a232a8ba3a269d0e6650
AAAlex-123 <alexmandelias@gmail.com>
2021-11-08 16:33:52 +0200

    implement First Choice HC

--- Exercise1/N_Queen_Problem/StateSearcher.cpp
+++ Exercise1/N_Queen_Problem/StateSearcher.cpp
@@ -26,3 +26,31 @@ State *StateSearcher::HillClimbing(State *initial_state)
 	return current;
 }
 
+State* StateSearcher::HCFirstChoice(State *initial_state)
+{
+	State *current = initial_state;
+
+	bool exists_larger = true;
+	while (exists_larger) {
+
+		exists_larger = false;
+		State *largest = current;
+
+		for (auto iter = current->begin(), end = current->end(); iter != end; iter++) {
+			if ((*iter)->getScore() > largest->getScore()) {
+				delete largest;
+				largest = *iter;
+				exists_larger = true;
+				break;
+			}
+			else {
+				delete* iter;
+			}
+		}
+		current = largest;
+	}
+
+	return current;
+}
+
+
