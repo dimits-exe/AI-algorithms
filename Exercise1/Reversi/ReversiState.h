3d0467ca7c4ede4fdc85b814ac1331a9231891ce
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2021-11-08 19:02:00 +0200

    Build interface

--- Exercise1/Reversi/ReversiState.h
+++ Exercise1/Reversi/ReversiState.h
@@ -1,22 +1,47 @@
 #pragma once
+#include <memory>
 #include <list>
-#include "Board.h"
 
-using namespace std;
+#include "AbstractState.h"
+#include "Board.h"
 
 class ReversiState {
 
 public:
-	ReversiState(Board& board);
-	~ReversiState();
+	ReversiState(PLAYER p, Board& board);
 
-	ReversiState& getFather();
-	void setFather(ReversiState&);
-	bool isFinal();
-	void makeMove(Move&);
+	ReversiState(const ReversiState&);
+
+	bool operator==(const AbstractState& other) {
+		return this->hashCode() == other.hashCode();
+	}
+
+	std::list<ReversiState> getChildren() const;
+
+	double hashCode() const;
+
+	bool isFinal() const;
+
+	Board getBoardCopy() const {
+		return board;
+	}
+
+	int getValue() const {
+		return value;
+	}
 
 private:
-	ReversiState& father;
-	Board board;
+	/*Used in the constructor.*/
+	static int calculateValue(ReversiState* state);
+
+	static const int BOARD_SIZE = 8;
+
+	std::unique_ptr<const AbstractState> father;
+
+	const int value;
+
+	const Board board;
+
+	const PLAYER turn;
 };
 
