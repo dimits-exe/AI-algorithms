5e5025ed3a5868cc6a70789395624b78490668de
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2021-11-16 20:10:06 +0200

    General changes

--- Exercise1/Reversi/ReversiState.cpp
+++ Exercise1/Reversi/ReversiState.cpp
@@ -2,28 +2,48 @@
 
 using namespace std;
 
-ReversiState::ReversiState(PLAYER p, Board& other_board): board(other_board), turn(p), value(calculateValue(this)) {}
+ReversiState::ReversiState(PLAYER p, const Board& other_board): board(other_board), turn(p), value(calculateValue(this)) {}
 
 ReversiState::ReversiState(const ReversiState& other): board(other.board), turn(nextTurn(other.turn)), value(other.value) {}
+
+Board ReversiState::getBoard() const {
+	return board;
+}
+
+void ReversiState::setFather(const ReversiState* father) {
+	this->father = father;
+}
+
+bool ReversiState::operator==(const ReversiState& other) const {
+	return this->hashCode() == other.hashCode();
+}
 
 double ReversiState::hashCode() const {
 	return board.hashcode();
 }
 
+size_t ReversiState::HashFunction::operator()(const ReversiState& state) const {
+	return state.hashCode();
+}
+
 bool ReversiState::isFinal() const {
-	return board.getValidMoves(turn).size() == 0; //current turn or next turn?
+	return (board.getValidMoves(this->turn).size() == 0) &&
+		(board.getValidMoves(nextTurn(this->turn)).size() == 0); 
+}
+
+int ReversiState::getValue() const {
+	return value;
 }
 
 int ReversiState::calculateValue(ReversiState* state) {
 	int score = state->board.getScore(state->turn);
-
 	if (state->isFinal()) { //if game won return infinity depending on who won
 		if(score > 0)
 			return INT_MAX;
 		else
 			return INT_MIN;
 	}
-	else {
+	else {					//else return normal score
 		return score;
 	}
 }
@@ -32,9 +52,13 @@ list<ReversiState> ReversiState::getChildren() const {
 	list<ReversiState> children;
 
 	for (Position move : board.getValidMoves(turn)) {
-		Board newBoard(BOARD_SIZE);
+		Board newBoard(this->board);
 		newBoard.makeMove(turn, move);
-		children.push_back(ReversiState(nextTurn(turn), newBoard));
+
+		ReversiState* newState = new ReversiState(nextTurn(turn), newBoard);
+		newState->setFather(this); 
+
+		children.push_back(*newState);
 	}
 
 	return children;
