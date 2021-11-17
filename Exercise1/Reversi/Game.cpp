c4d6d50f914733aa538d493095584a8e95028ed0
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2021-11-17 20:55:27 +0200

    Update Game.cpp

--- Exercise1/Reversi/Game.cpp
+++ Exercise1/Reversi/Game.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include "Minimax.h"
 
-
+#define INPUT_SYMBOL    	"\n>"
 #define WELCOME_MSG		"Welcome to Reversi! This is a singleplayer game where you get against an AI"
-#define SELECT_DEPTH	"Please select the depth of the algorithm"
-#define SELECT_TURN		"Do you want to play first? (y/n)"
-#define MAX_VALID_DEPTH	20
+#define SELECT_DEPTH		"Please select the depth of the algorithm" INPUT_SYMBOL
+#define SELECT_TURN		"Do you want to play first? (y/n)" INPUT_SYMBOL
+#define MAKE_MOVE		"Play the next move (Give x-y coordinates)." INPUT_SYMBOL
+#define WRONG_MOVE		"This move is invalid."
+#define MAX_VALID_DEPTH		20
 
 using namespace std;
+
+Board makePlayerMove(PLAYER turn, const Board* currentBoard);
 
 int main(void) {
 	cout << WELCOME_MSG << endl;
@@ -24,29 +28,45 @@ int main(void) {
 		cin >> answer;
 	} while (answer != "y" && answer != "n" && answer != "Y" && answer != "N");
 
-	PLAYER turn;
+	PLAYER CPU;
 	if (answer == "y" || answer == "Y")
-		turn = PLAYER::PLAYER1;
+		CPU = PLAYER::PLAYER2;
 	else
-		turn = PLAYER::PLAYER2;
+		CPU = PLAYER::PLAYER1;
+
+	PLAYER turn = PLAYER::PLAYER1;
 
 	Board* gameBoard = new Board(8);
 	while (gameBoard->getValidMoves(PLAYER::PLAYER1).size() != 0 &&
 		gameBoard->getValidMoves(PLAYER::PLAYER2).size() != 0) { //crashes here, board's array size is 0
 
-		Board board = mini_max(turn, *gameBoard, depth);
+		Board* newBoard;
+		if (turn == CPU)
+			newBoard = &mini_max(turn, *gameBoard, depth);
+		else
+			makePlayerMove(turn, gameBoard);
+
 		delete(gameBoard);
-		gameBoard = &board;
+		gameBoard = newBoard;
 		turn = nextTurn(turn);
 	}
 
 	return 0;
 }
 
-/*
-	Board board(8);
-	cout << mini_max(PLAYER::PLAYER1, board, 2).toString() << endl;
-
-	cout << mini_max(PLAYER::PLAYER2, board, 2).toString() << endl;
-
-	return 0;*/
+void makePlayerMove(PLAYER turn, Board* const currentBoard) {
+	int x, y;
+	bool succeded = true;
+	do {
+		cout << MAKE_MOVE << endl;
+		cin >> x >> y;
+		try {
+			currentBoard->makeMove(turn, Position(x, y));
+			succeded = true;
+		}
+		catch (logic_error) {
+			cout << WRONG_MOVE << endl;
+			succeded = false;
+		}
+	} while (!succeded);
+}
