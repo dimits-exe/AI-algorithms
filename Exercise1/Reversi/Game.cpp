2286951b1b18ccccc0a4c20d29c49938bffb993f
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2021-11-21 20:32:29 +0200

    Properly implement game

--- Exercise1/Reversi/Game.cpp
+++ Exercise1/Reversi/Game.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
 #include "Minimax.h"
 
-#define INPUT_SYMBOL    	"\n>"
-#define WELCOME_MSG		"Welcome to Reversi! This is a singleplayer game where you get against an AI"
+#define INPUT_SYMBOL		"\n>"
+#define WELCOME_MSG			"Welcome to Reversi! This is a singleplayer game where you get against an AI"
 #define SELECT_DEPTH		"Please select the depth of the algorithm" INPUT_SYMBOL
-#define SELECT_TURN		"Do you want to play first? (y/n)" INPUT_SYMBOL
-#define MAKE_MOVE		"Play the next move (Give x-y coordinates)." INPUT_SYMBOL
-#define WRONG_MOVE		"This move is invalid."
+#define SELECT_TURN			"Do you want to play first? (y/n)" INPUT_SYMBOL
+#define MAKE_MOVE			"Play the next move (Give x-y coordinates)." INPUT_SYMBOL
+#define WRONG_MOVE			"This move is invalid."
 #define MAX_VALID_DEPTH		20
 
 using namespace std;
 
-Board makePlayerMove(PLAYER turn, const Board* currentBoard);
+void makePlayerMove(PLAYER turn, Board& currentBoard);
 
 int main(void) {
 	cout << WELCOME_MSG << endl;
 
-	int depth;
+	int depth = -1;
 	do {
-		cout << SELECT_DEPTH << endl;
+		cout << SELECT_DEPTH;
 		cin >> depth;
 	} while (depth <0 && depth > MAX_VALID_DEPTH);
 
 	string answer;
 	do {
-		cout << SELECT_TURN << endl;
+		cout << SELECT_TURN;
 		cin >> answer;
 	} while (answer != "y" && answer != "n" && answer != "Y" && answer != "N");
 
@@ -34,39 +34,52 @@ int main(void) {
 	else
 		CPU = PLAYER::PLAYER1;
 
+	//initialize variables
 	PLAYER turn = PLAYER::PLAYER1;
+	Board gameBoard(8);
 
-	Board* gameBoard = new Board(8);
-	while (gameBoard->getValidMoves(PLAYER::PLAYER1).size() != 0 &&
-		gameBoard->getValidMoves(PLAYER::PLAYER2).size() != 0) { //crashes here, board's array size is 0
+	//main game loop
+	while (gameBoard.getValidMoves(PLAYER::PLAYER1).size() != 0 &&
+		gameBoard.getValidMoves(PLAYER::PLAYER2).size() != 0) { //crashes here, board's array size is 0
 
-		Board* newBoard;
 		if (turn == CPU)
-			newBoard = &mini_max(turn, *gameBoard, depth);
+			gameBoard = mini_max(turn, gameBoard, depth);
 		else
 			makePlayerMove(turn, gameBoard);
 
-		delete(gameBoard);
-		gameBoard = newBoard;
 		turn = nextTurn(turn);
 	}
+
+	cout << "Player "<< static_cast<char>(turn) << " won!" << endl;
 
 	return 0;
 }
 
-void makePlayerMove(PLAYER turn, Board* const currentBoard) {
+void makePlayerMove(PLAYER turn, Board& currentBoard) {
 	int x, y;
 	bool succeded = true;
+
+	cout << currentBoard.toString();
 	do {
-		cout << MAKE_MOVE << endl;
-		cin >> x >> y;
-		try {
-			currentBoard->makeMove(turn, Position(x, y));
-			succeded = true;
-		}
-		catch (logic_error) {
+		cout << MAKE_MOVE;
+		cin >> y >> x;
+		x--; y--; //convert to 1-8 range
+		cout << "Player " << static_cast<char>(turn) << "played " << y << '-' << x << endl;
+
+		if (x <= 0 || x > 8 || y <= 0 || y > 8) {
 			cout << WRONG_MOVE << endl;
 			succeded = false;
 		}
+		else {
+			try {
+				currentBoard.makeMove(turn, Position(x, y));
+				succeded = true;
+			}
+			catch (logic_error) {
+				cout << WRONG_MOVE << endl;
+				succeded = false;
+			}
+		}
+
 	} while (!succeded);
 }
