d1743e45a175f2041798c79a4f83e89e3122e70d
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2021-11-24 18:17:26 +0200

    Update Minimax.h

--- Exercise1/Reversi/Minimax.h
+++ Exercise1/Reversi/Minimax.h
@@ -2,6 +2,16 @@
 #include <unordered_set>
 #include "ReversiState.h"
 
+
+struct Info {
+	int x_of_move = -1;
+	int y_of_move = -1;
+	const int MAX_DEPTH;
+	const Board original;
+
+	Info(Board original, int maxDepth): original(original), MAX_DEPTH(maxDepth) {}
+};
+
 /// <summary>
 /// Runs the board through an AI algorithm to determine the best move.
 /// </summary>
@@ -9,7 +19,7 @@
 /// <param name="start">The current state of the board</param>
 /// <param name="max_depth">The maximum search depth for the algorithm.</param>
 /// <returns>A new board with the best move marked.</returns>
-Board mini_max(PLAYER turn, const Board& start, int max_depth);
+Position mini_max(PLAYER turn, const Board& start, int max_depth);
 
 /// <summary>
 /// The 'min' half of the alpha-beta pruning algorithm.
@@ -20,7 +30,7 @@ Board mini_max(PLAYER turn, const Board& start, int max_depth);
 /// <param name="b">The parameter of the node's min value</param>
 /// <param name="TTL">How many more calls will be made before the algorithm terminates.</param>
 /// <returns>The state with the current best outcome for the computer.</returns>
-static ReversiState min_value(std::unordered_set <ReversiState, ReversiState::HashFunction>& closed_set, 
+static int min_value(Info& context, std::unordered_set <ReversiState, ReversiState::HashFunction>& closed_set,
 	const ReversiState& state, int a, int b, int TTL);
 
 /// <summary>
@@ -32,5 +42,5 @@ static ReversiState min_value(std::unordered_set <ReversiState, ReversiState::Ha
 /// <param name="b">The parameter of the node's min value</param>
 /// <param name="TTL">How many more calls will be made before the algorithm terminates.</param>
 /// <returns>The state with the current best outcome for the computer.</returns>
-static ReversiState max_value(std::unordered_set <ReversiState, ReversiState::HashFunction>& closed_set,
+static int max_value(Info& context, std::unordered_set <ReversiState, ReversiState::HashFunction>& closed_set,
 	const ReversiState& state, int a, int b, int TTL);
