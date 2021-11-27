937a13fc5d6879113dcd0c10cf09a18c78c6d22e
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2021-11-27 13:31:19 +0200

    Remove debug, streamline methods

--- Exercise1/Reversi/Minimax.cpp
+++ Exercise1/Reversi/Minimax.cpp
@@ -1,24 +1,24 @@
 #include "Minimax.h"
 #include <limits>
 #include <math.h>
-#include <iostream> //delete this
 
 
 using namespace std;
 
-Position mini_max(PLAYER turn, const Board& starting_board, int max_depth) {
+Info::Info(int maxDepth):MAX_DEPTH(maxDepth), move_made(Position(-1, -1)) {}
+
+Position mini_max(PLAYER turn, const Board starting_board, int max_depth) {
 	unordered_set <ReversiState, ReversiState::HashFunction> state_set;
-	ReversiState start(turn, turn, starting_board);
-	Info context(starting_board, max_depth+1);
+	ReversiState start(nextTurn(turn), turn, starting_board);
+	Info context( max_depth);
 
-	max_value(context, state_set, start, INT_MIN, INT_MAX, max_depth+1);
+	max_value(context, state_set, start, INT_MIN, INT_MAX, max_depth);
 
-	return Position(context.x_of_move, context.y_of_move);
+	return context.move_made;
 }
 
 int min_value(Info& context, unordered_set <ReversiState, ReversiState::HashFunction>& closed_set, 
 	const ReversiState& state, int a, int b, int TTL) {
-	//cout << "Min " << static_cast<char>(state.turn) << endl;
 
 	//if the game is predicted to end or if the state was already examined or the recursion has to end
 	if (state.isFinal() || TTL < 0 || closed_set.count(state) == 1)
@@ -31,24 +31,20 @@ int min_value(Info& context, unordered_set <ReversiState, ReversiState::HashFunc
 	const list<ReversiState>& children = state.getChildren();
 
 	if (children.size() == 0) {
-		//cout << "No possible moves " << '\n' << state.getBoard().toString() << endl;
 		return max_value(context, closed_set, state, a, b, TTL - 2);
 	}
 
 	//examine all children and find the least fit one
 	for (const ReversiState& child : children) {
 		int max_v = max_value(context, closed_set, child, a, b, TTL - 2);
-		//cout << "\t\t Needed value less than " << value << " value got: " << max_v << endl;
 		if (value > max_v) {
 			value = max_v;
 			min_value_state = &child;
 		}
 
 		if (value <= a) {
-			//cout << "Pruned " << '\n' << state.getBoard().toString() << endl;
 			return child.getValue();
 		}
-			
 
 		b = min(b, value);
 	}
@@ -57,7 +53,6 @@ int min_value(Info& context, unordered_set <ReversiState, ReversiState::HashFunc
 		throw logic_error("No suitable state found");
 	}
 	else {
-		//cout << "Normal return from min " << '\n' << state.getBoard().toString() << " with value " << state.getValue() << endl;
 		return min_value_state->getValue();
 	}
 		
@@ -65,15 +60,9 @@ int min_value(Info& context, unordered_set <ReversiState, ReversiState::HashFunc
 
 int max_value(Info& context, unordered_set <ReversiState, ReversiState::HashFunction>& closed_set, 
 	const ReversiState& state, int a, int b, int TTL) {
-	//cout << "Max " << static_cast<char>(state.turn) << endl;
 
-	//cout << "Max depth: " << context.MAX_DEPTH << " TTL: " << TTL << endl;
-	if (context.MAX_DEPTH == TTL + 2) { //if directly below the root of the search tree
-		const Position& move_made = state.getLastMove();
-		//cout << state.getBoard().toString() << endl;
-		//cout << "X = " << move_made.X() << "Y = " << move_made.Y() << endl;
-		context.x_of_move = move_made.X();
-		context.y_of_move = move_made.Y();
+	if (context.MAX_DEPTH == TTL + 2) {				//if directly below the root of the search tree
+		context.move_made = state.getLastMove();	//save the move made
 	}
 
 	//if the game is predicted to end or if the state was already examined or the recursion has to end
@@ -88,24 +77,20 @@ int max_value(Info& context, unordered_set <ReversiState, ReversiState::HashFunc
 	const list<ReversiState>& children = state.getChildren();
 
 	if (children.size() == 0) {
-		//cout << "No possible moves " << '\n' << state.getBoard().toString() << endl;
 		return min_value(context,closed_set, state, a, b, TTL);
 	}
 
 	//examine all children and find the fittest one
 	for (const ReversiState& child : children) {
 		int min_v = min_value(context, closed_set, child, a, b, TTL);
-		//cout << "\t\t Needed value bigger than " << value << " value got: " << min_v << endl;
 		if (value < min_v) {
 			value = min_v;
 			max_value_state = &child;
 		}
 
 		if (value >= b) {
-			//cout << "Pruned " << '\n' << state.getBoard().toString() << endl;
 			return child.getValue();
 		}
-
 
 		a = max(a, value);
 	}
@@ -114,7 +99,6 @@ int max_value(Info& context, unordered_set <ReversiState, ReversiState::HashFunc
 		throw logic_error("No suitable state found");
 	}
 	else {
-		//cout << "Normal return from max " << '\n' << state.getBoard().toString() << " with value " << state.getValue() << endl;
 		return max_value_state->getValue();
 	}
 
