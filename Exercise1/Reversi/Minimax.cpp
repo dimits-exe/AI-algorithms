bda15a493eeb1c3a472448758ee6e2363091893a
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2021-11-27 19:56:35 +0200

    Taught the AI that the best move is -in fact- best

--- Exercise1/Reversi/Minimax.cpp
+++ Exercise1/Reversi/Minimax.cpp
@@ -2,17 +2,17 @@
 #include <limits>
 #include <math.h>
 
-
 using namespace std;
 
-Info::Info(int maxDepth):MAX_DEPTH(maxDepth), move_made(Position::create_invalid()) {}
+Info::Info(int maxDepth):MAX_DEPTH(maxDepth), move_made(Position::create_invalid()), best_value(INT_MIN) {}
 
+//I'm so proud of my boy, Im naming him Stockfish Jr
 Position mini_max(PLAYER turn, const Board starting_board, int max_depth) {
 	unordered_set <ReversiState, ReversiState::HashFunction> state_set;
-	ReversiState start(nextTurn(turn), turn, starting_board);
-	Info context( max_depth);
+	ReversiState start(turn, turn, starting_board);
+	Info context(max_depth);
 
-	max_value(context, state_set, start, INT_MIN, INT_MAX, max_depth);
+	min_value(context, state_set, start, INT_MIN, INT_MAX, max_depth);
 
 	return context.move_made;
 }
@@ -50,7 +50,7 @@ int min_value(Info& context, unordered_set <ReversiState, ReversiState::HashFunc
 	}
 
 	if (min_value_state == nullptr) {
-		throw logic_error("No suitable state found");
+		return state.getValue();
 	}
 	else {
 		return min_value_state->getValue();
@@ -61,19 +61,21 @@ int min_value(Info& context, unordered_set <ReversiState, ReversiState::HashFunc
 int max_value(Info& context, unordered_set <ReversiState, ReversiState::HashFunction>& closed_set, 
 	const ReversiState& state, int a, int b, int TTL) {
 
+	Position AI_move;
 	if (context.MAX_DEPTH == TTL + 2) {				//if directly below the root of the search tree
-		context.move_made = state.getLastMove();	//save the move made
+		AI_move = state.getLastMove();				//save the move made
 	}
 
 	//if the game is predicted to end or if the state was already examined or the recursion has to end
-	if (state.isFinal() || TTL < 0 || closed_set.count(state) == 1)
-		return state.getValue();
+	if (state.isFinal() || TTL < 0 || closed_set.count(state) == 1) {
+		int value = state.getValue();
+		store_move_if_best(context, AI_move, value, TTL);
+		return value;
+	}
 
 	closed_set.insert(state);
 	int value = INT_MIN;
 	const ReversiState* max_value_state = nullptr; //will point to the state with the max value
-
-
 	const list<ReversiState>& children = state.getChildren();
 
 	if (children.size() == 0) {
@@ -89,6 +91,7 @@ int max_value(Info& context, unordered_set <ReversiState, ReversiState::HashFunc
 		}
 
 		if (value >= b) {
+			store_move_if_best(context, AI_move, value, TTL);
 			return child.getValue();
 		}
 
@@ -96,10 +99,19 @@ int max_value(Info& context, unordered_set <ReversiState, ReversiState::HashFunc
 	}
 
 	if (max_value_state == nullptr) {
-		throw logic_error("No suitable state found");
+		return state.getValue();
 	}
 	else {
+		store_move_if_best(context, AI_move, value, TTL);
 		return max_value_state->getValue();
 	}
 
+}
+
+void store_move_if_best(Info& context, Position& AI_move, int move_value, int curr_depth) {
+	if (context.MAX_DEPTH == curr_depth + 2 && move_value > context.best_value) {
+		context.move_made = AI_move;
+		context.best_value = move_value;
+	}
+		
 }
