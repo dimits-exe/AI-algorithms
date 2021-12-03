b12f9df82f8850d1443e353573248516bbb9cef1
AAAlex-123 <alexmandelias@gmail.com>
2021-12-03 21:29:01 +0200

    fix main with commandline args

--- Exercise1/N_Queen_Problem/main.cpp
+++ Exercise1/N_Queen_Problem/main.cpp
@@ -1,77 +1,81 @@
+#define _SILENCE_CXX17_ITERATOR_BASE_CLASS_DEPRECATION_WARNING
+
+#include <cstring>
 
 #include "State.h"
 #include "StateSearcher.h"
 
 using namespace std;
 
-int main() {
-	//int size = 5;
-	//bool* data = new bool[size * size]{
-	//	0, 0, 0, 1, 0,
-	//	0, 0, 0, 1, 0,
-	//	0, 0, 1, 0, 0,
-	//	0, 1, 0, 0, 0,
-	//	0, 0, 1, 0, 0,
-	//};
-	//bool* data1 = new bool[size * size]{
-	//	1, 0, 0, 0, 0,
-	//	0, 0, 0, 1, 0,
-	//	0, 0, 0, 0, 1,
-	//	0, 1, 0, 0, 0,
-	//	0, 0, 1, 0, 0,
-	//};
+int main(int argc, char** argv) {
 
-	//State state = State(size, data);
-	//State s2 = State(size, data);
-	//State s3 = State(size, data1);
-	//state.evaluate();
-	//s2.evaluate();
-	//s3.evaluate();
-
-	//vector<State*> *v = state.get_children();
-	//bool final = state.isFinal();
-	//int score = state.getScore();
-	//bool same1 = state == s2;
-	//bool same2 = state == s3;
-	//size_t hash = State::HashFunction()(state);
-	//size_t hash3 = State::HashFunction()(s3);
-
-	//state.print();
-	//cout << (*v).size() << endl << final << endl <<
-	//	score << endl << same1 << endl << same2 << endl <<
-	//	hash << endl << hash3 << endl;
-
-	//State* s = State::random(4);
-	//s->print();
-	//cout << endl;
-
+	// initialize the random seed that is used by some HC algorithms
 	srand((unsigned)time(NULL));
 
-	auto res = [](string name, State* s) {
-		cout << name;
-		printf(": %d\n", s->getScore());
+	// parse commandline arguments
+	if (argc < 2) {
+		cout << "Usage: n_queens <size> [options]" << endl << endl
+			<< "Where options include:" << endl
+			<<"\t -hc for HillClimbing" << endl
+			<<"\t -fc for FirstChoice" << endl
+			<<"\t -rr for RandomRestarts with 5 restarts (hardcoded)" << endl
+			<<"\t -st for Stochastic" << endl;
+		return -1;
+	}
+
+	int size = atoi(argv[1]);
+
+	if (size < 3) {
+		cout << "Invalid size! It must be an integer greater than or equal to 3" << endl;
+		return -1;
+	}
+
+	bool hc = true, fc = true, rr = true, st = true;
+	if (argc > 2) {
+		hc = fc = rr = st = false;
+
+		for (int i = 2; i < argc; i++) {
+			// cout << "'" << argv[i] << "'" << endl;
+			hc |= strcmp(argv[i], "-hc") == 0;
+			fc |= strcmp(argv[i], "-fc") == 0;
+			rr |= strcmp(argv[i], "-rr") == 0;
+			st |= strcmp(argv[i], "-st") == 0;
+		}
+	}
+	// cout << hc << fc << rr << st << endl;
+
+	// run the algorithms according to commandline args
+	State* start = nullptr, * hc_state = nullptr, * fc_state = nullptr, * rr_state = nullptr, * st_state = nullptr;
+
+	start = State::random(size);
+
+	if (hc)
+		hc_state = StateSearcher::HillClimbing(start);
+	if (fc)
+		fc_state = StateSearcher::HCFirstChoice(start);
+	if (rr)
+		rr_state = StateSearcher::HCRandomRestarts(size, 5);
+	if (st)
+		st_state = StateSearcher::HCStochastic(start);
+
+	// declare `results` function for printing results and cleanup
+	auto results = [](string name, State* s) {
+		cout << name << ": " << s->getScore() << " max: " << s->max_score << endl;
 		s->print();
 		delete s;
 	};
 
-	int times = 1;
-	while (times--) {
-		int dim = 50;
-		State* s1 = State::random(dim);
-		s1->print();
+	// print results according to commandline args
+	results("Starting state", start);
+	if (hc)
+		results("HC", hc_state);
+	if (fc)
+		results("HCFC", fc_state);
+	if (rr)
+		results("HCRR", rr_state);
+	if (st)
+		results("HCSt", st_state);
 
-		State* f1 = StateSearcher::HillClimbing(s1);
-		State* f2 = StateSearcher::HCFirstChoice(s1);
-		State* f3 = StateSearcher::HCRandomRestarts(dim, 3);
-		State* f4 = StateSearcher::HCStochastic(s1);
-
-		res("start", s1);
-		res("HC", f1);
-		res("HCFC", f2);
-		res("HCRR", f3);
-		res("HCSt", f4);
-	}
-	times++;
+	cout << endl;
 }
-
 
