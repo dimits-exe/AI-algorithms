7acad6d76d599dcf020b9a0a369b90acfe2ded94
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-12-25 03:00:17 +0200

    fix the spaghet

--- Exercise2/load_imdb.py
+++ Exercise2/load_imdb.py
@@ -1,86 +1,87 @@
 import os, re
 
 class WordInfo:
-    """
-    A class holding information about the use of an associated word.
-    """
+    """ A class holding information about the frequency of a word """
 
-    def __init__(self):
+    def __init__(self, word):
+        self._word = word   # not used but could be there for completeness
         self.positive_occurences = 0
         self.frequency = 1
 
     def get_positive_chance(self):
         """
-        Get the chance the word is used in a positive context.
-        For example if a word is used 10 times, three of which in a positive 
-        review, the method would return 0.3
+        Returns the chance the word is used in a positive context. For example
+        if a word is used 10 times, three of which occurred a positive review,
+        the method would returns 0.3
 
         Returns:
-            A float in the [0,1] range or None if the word was never used.
+            A float between 0 and 1
         """
-        if self.frequency == 0:
-            return None
-        else:
-            return self.positive_occurences / self.frequency
+        return self.positive_occurences / self.frequency
 
     def __str__(self):
-        return str(self.get_positive_chance())[:5] + "%"
+        return "%s - %d - %d - %.5f" % (self._word, self.positive_occurences,
+                self.frequency, self.get_positive_chance())
 
-def get_training_data(directory_path, ignored_words_count, max_words_count, sample_size = 5000):
+def get_training_data(dir_path, ignored_count, max_count, sample_size = 5000):
     """
-    Read the data file and return a dictionary with all the words contained within,
-    ignoring the N most common words.
+    Reads text from the files in a directory and returns a dictionary with the
+    M most frequent words and their wordinfo, ignoring the N most common ones.
 
     Parameters:
-        directory_path (string): The path to the directory containing ALL the training data files
-        ignored_words_count (int): The N most common words not to be included in the dictionary
-        max_words_count (int): The max number of most common words stored in the dictionary
-        sample_size (int): The max numbers of files read
-
-    Throws:
-        ValueError if the int arguments are negative or max_words_count is smaller than
-        ignored_words_count (an empty dictionary would always be returned)
+        dir_path (str): The path to the directory with all the training data
+        ignored_count (int): How many of the most common words will be excluded
+        max_count (int): How many words will be included
+        sample_size (int): The maximum numbers of files read
 
     Returns:
-        (dict) A dictionary with [word (string)] : [chance_of_being_positive (float)] records
+        (dict) A dictionary with [word (str)] : [word_info (WordInfo)] records
+
+    Throws:
+        ValueError: if the ignored and max counts are negative
     """
-    ignored_tokens_regex = re.compile("\"|,>|<|\\|/|-")
 
-    #check parameters
-    if ignored_words_count < 0 or max_words_count <= 0:
-        raise ValueError("The calling parameters need to be positive")
-    if max_words_count < ignored_words_count:
-        raise ValueError("The returned word count must be higher than the ignored word count")
+    # validate parameters
+    if ignored_count < 0 or max_count <= 0:
+        raise ValueError("The max- and ignored-word-counts must be positive")
 
-    #gather data
+    # gather data
     training_data = dict()
+    neg_dir_path = os.path.join(dir_path, "neg")
+    pos_dir_path = os.path.join(dir_path, "pos")
+    ignored_chars = ['"', "'", '.', ',', '>', '<', '\\', '/', '-', '(', ')',
+            ';', ':', '?']
+
     print("Loading postive training data...")
-    _read_reviews(training_data, os.path.join(directory_path, "neg"), False, 
-        sample_size // 2, ignored_tokens_regex)
+    _read_reviews(training_data, neg_dir_path, False, sample_size // 2,
+        ignored_chars)
 
     print("Loading negative training data...")
-    _read_reviews(training_data, os.path.join(directory_path, "pos"), True, 
-        sample_size // 2, ignored_tokens_regex)
+    _read_reviews(training_data, pos_dir_path, True, sample_size // 2,
+        ignored_chars)
 
     print("Loading succesfull.")
+    
+    if len(training_data) <= ignored_count:
+        return training_data
 
-    #get the N most common words
-    most_common_words = set(sorted(list(training_data.values()), 
-        key= lambda x: x.frequency)[ignored_words_count:])
-    #is this how Im supposed to write code in this cursed language?
+    # is this how Im supposed to write code in this cursed language? no, *this* is :sunglasses:
+    # TODO: remove this comment after getting flexed on by superior code. more :sunglasses:
 
-    #filter results
-    filtered_data = dict()
+    # sort all the frequencies to find the upper and lower threshold for words
+    frequencies = [info.frequency for info in training_data.values()]
+    sorted_frequencies = sorted(frequencies, reverse=True) # biggest to smallest
+    frequency_count = len(sorted_frequencies)
+    max_count = min(max_count, frequency_count - ignored_count)
+    
+    # start from `ignored_count` up to `ignored_count + max_count`
+    # both thresholds are included in the range of acceptable frequencies
+    upper_threshold = sorted_frequencies[ignored_count]
+    lower_threshold = sorted_frequencies[ignored_count + max_count - 1]
 
-    for data_item in training_data.items():
-        if len(training_data) <= 0 or len(filtered_data) > max_words_count:
-            break
-
-        if data_item not in most_common_words:
-            filtered_data[data_item[0]] = data_item[1]
-
-    return filtered_data
-
+    # return filtered results, k = word, v = wordinfo
+    check_threshold = lambda freq: lower_threshold <= freq <= upper_threshold
+    return {k:v for k, v in training_data.items() if check_threshold(v.frequency)}
 
 def get_testing_data(directory_path):
     """
@@ -92,76 +93,84 @@ def get_testing_data(directory_path):
     Returns:
         (dict) A dictionary with [review (string)] : [is_positive (bool)] records
     """
-    
 
-def _read_reviews(dictionary, directory_path, is_positive, sample_size, ignored_tokens_regex):
+def _read_reviews(wordinfo_dict, dir_path, is_positive, sample_size, ignored_chars):
     """
-    Open all files in the given directory and append [word] : [WordInfo]
-    records in the provided dictionary.
+    Opens all files in a directory and appends [word] : [WordInfo] records in
+    the provided dictionary.
 
     Parameters:
-        dictionary (dict): The dictionary in which the results of the file scan will be appended to.
-        directory_path (string): The path to the directory containing the
-        positive / negative testing data files
-        is_positive (bool): Whether or not the given directory contains positive reviews
-        ignored_tokens_regex (Pattern): a pattern which is used to clean strings of impure content
-        such as dots, commas, etc
-
-    Throws:
-        OSError if the directory isn't valid
+        wordinfo_dict (dict): The dictionary to which the results of the file
+            scan will be added.
+        dir_path (str): The directory containing the training data
+        is_positive (bool): Indicates if the directory contains positive reviews
+        ignored_chars (list): The characters that will be removed from the text
     """
-    def clean_string(string): #TODO: implement correct regex
-        #return ignored_tokens_regex.sub(string, "")
-        return string
 
-    files_read = 0
-    percent_progress = 0
+    files = os.listdir(dir_path)
+    ignored_chars_pattern = re.compile("[%s\\d]" % (re.escape("".join(ignored_chars))))
+    one_tenth_progress = min(len(files), sample_size) // 10
+    
+    for count, file in enumerate(files):
+        # check if file limit has been reached
+        if count == sample_size:
+            break
 
-    files = os.listdir(directory_path)
-    for file in files:
         # read file contents
         try:
-            f_handle = open(os.path.join(directory_path, file), mode='r', encoding= "utf8")
+            file_path = os.path.join(dir_path, file)
+            f_handle = open(file_path, mode='r', encoding= "utf8")
             contents = f_handle.read().lower()
-        except PermissionError:
-            print("Ignored file " + file + " because of missing permissions")
-        except os.error as err: #ignore all files with errors
-            print("Ignored file " + file + " because of " + str(err))
         finally:
             f_handle.close()
 
-        for word in contents.split():
-            word = clean_string(word)
+        # clean contents
+        clean_contents = ignored_chars_pattern.sub("", contents, 0)
+        clean_contents = re.sub("\\s+", " ", clean_contents)
 
-            if word not in dictionary:
-                dictionary[word] = WordInfo()   #create record
-            else:
-                dictionary[word].frequency += 1 #update record
+        # for each text that changes look if everything is ok regarding ignored
+        # TODO: remove
+        """
+        print(contents)
+        print()
+        print(clean_contents)
+        input()
+        print()
+        """
+
+        # update frequencies
+        for word in clean_contents.split(" "):
+            wordinfo = wordinfo_dict.setdefault(word, WordInfo(word))
+            wordinfo.frequency += 1
             if is_positive:
-                dictionary[word].positive_occurences += 1
-        files_read += 1
+                wordinfo.positive_occurences += 1
 
-        #show % progress
-        if files_read % (min(len(files), sample_size) // 10) == 0:
-            print(percent_progress*10, "% complete...")
-            percent_progress += 1
+        # show % progress
+        if (count + 1) % one_tenth_progress == 0:   # `count` is 0-indexed
+            print("%d%% complete..." % ((count + 1) // one_tenth_progress * 10))
 
-        #check if file limit is reached
-        if files_read > sample_size:
-            break
-
-if __name__ == "__main__":
+def main():
     import time
 
-    p = os.path
-    #search in the current directory
-    directory = p.join(p.join(p.dirname(p.abspath(__file__)), "aclImdb"), "train")
+    path_to_file = os.path.dirname(os.path.abspath(__file__))
+    directory = os.path.join(path_to_file, "aclImdb", "train")
 
-    start = time.time()
-    results = get_training_data(directory, 20, 50, 1000)
-    end = time.time()
+    try:
+        start = time.perf_counter()
+        results = get_training_data(directory, 5, 10, 10000)
+        end = time.perf_counter()
 
-    for item in results.items():
-        print(item[0], item[1])
+    except os.error as err:
+        print(f"Process didn't complete normally due to: {err}")
 
-    print("Process ended after ", end - start, " seconds.")
+    else:
+        get_freq = lambda item: item[1].frequency
+        items_by_freq = sorted(results.items(), key = get_freq, reverse=True)
+
+        for word, wordinfo in items_by_freq:
+            print(word, wordinfo.frequency)
+
+        print(f"Process ended after {end-start} seconds.")
+
+if __name__ == "__main__":
+    main()
