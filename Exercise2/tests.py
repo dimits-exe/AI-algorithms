0efc2105905ea76b6d976df3e929f272c7c60b17
Alex Mandelias <alexmandelias@gmail.com>
2022-01-28 13:54:22 +0200

    add entropy test

--- Exercise2/tests.py
+++ Exercise2/tests.py
@@ -1,7 +1,10 @@
 import os
+
 from timed import timed
 
 from load_imdb import load_examples, load_attributes
+
+from id3 import entropy
 
 
 @timed(prompt="Test Load")
@@ -26,5 +29,24 @@ def test_load(sample_size: int, count: int, ignore: int) -> None:
     print(attributes, sep="\n")
 
 
+def test_entropy(*probabilities: float) -> list[float]:
+    """
+    Prints and returns the entropy for every probability provided and runs some hard-coded assertions
+
+    :param probabilities: the probabilities to calculate the entropy for
+    :return: a list that has the entropy of probability i at index i
+    """
+    entropies = [entropy(p) for p in probabilities]
+    for probability, probability_entropy in zip(probabilities, entropies):
+        print("H(%0.2f) = %0.5f" % (probability, probability_entropy))
+
+    assert entropy(1) == 0
+    assert entropy(0) == 0
+    assert entropy(1/2) == 1
+
+    return entropies
+
+
 if __name__ == '__main__':
-    test_load(5, 3, 10)
+    # test_load(5, 3, 10)
+    test_entropy()
